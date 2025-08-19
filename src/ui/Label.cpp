d5845a8b80fcd52f1c2b630c459c274df169217a
Michael Smith <mike@goswift.ly>
2025-08-18 17:37:46 -0700

    Fixed so can center label even for different font size.

--- src/ui/Label.cpp
+++ src/ui/Label.cpp
@@ -11,21 +11,24 @@ Label::Label() {
 	color = color::BLACK_TRANSPARENT;
 }
 
+
 void Label::draw(const DrawArgs& args) {
-	// TODO
-	// Custom font sizes do not work with right or center alignment
-	float x;
-	switch (alignment) {
-		default:
-		case LEFT_ALIGNMENT: {
-			x = 0.0;
-		} break;
-		case RIGHT_ALIGNMENT: {
-			x = box.size.x - bndLabelWidth(args.vg, -1, text.c_str());
-		} break;
-		case CENTER_ALIGNMENT: {
-			x = (box.size.x - bndLabelWidth(args.vg, -1, text.c_str())) / 2.0;
-		} break;
+    // For debugging one can draw rectangle so that can see the rectanble used for the Label
+    //bndBackgroundColor(args.vg, 0.0, 0.0, box.size.x, box.size.y, 0, color::RED, color::GREEN);
+
+    // Align the text as specified.
+    float x;
+    switch (alignment) {
+        default:
+        case LEFT_ALIGNMENT: {
+            x = 0.0;
+        } break;
+        case RIGHT_ALIGNMENT: {
+            x = box.size.x - bndLabelWidthForFontSize(args.vg, -1, fontSize, text.c_str());
+        } break;
+        case CENTER_ALIGNMENT: {
+            x = (box.size.x - bndLabelWidthForFontSize(args.vg, -1, fontSize, text.c_str())) / 2.0;
+        } break;
 	}
 
 	nvgTextLineHeight(args.vg, lineHeight);
