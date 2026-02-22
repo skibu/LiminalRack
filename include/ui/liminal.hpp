83fa6054b11f0f63b9733b6f4378e1f942a49eb7
Michael Smith (SynthBu) <msmithtransit@gmail.com>
2026-02-21 17:48:52 -0800

    Commented configAsLIminal()

--- include/ui/liminal.hpp
+++ include/ui/liminal.hpp
@@ -11,7 +11,17 @@ namespace ui {
 
 class Liminal {
    public:
-    /** Configures settings for Liminal mode. To be called at startup */
+    /** Configures settings for Liminal mode. To be called at startup.
+     * Overrides settings such as isLiminal, hasTouchscreen, and hasKeyboard.
+     * Also sets windowMaximized to true for Wayland since on other machines
+     * like Macs most likely full screen mode would interfere with other
+     * applications on the monitor, include IDE when debugging. Also increases
+     * font size.
+     * 
+     * Overrides the default values from the settings.cpp file. But then
+     * these values can be overridden once again when the settings.json file
+     * is read in.
+     */
     static void configAsLiminal();
 
     static bool isLiminal() {
